656e78ffc16f912c13d72bb13e260b66d631d46e
DuyDo <duydo.dev@gmail.com>
2017-10-11 20:21:02 -0500

    add controller.js

---
+++ controllers/controller.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var router = express.Router();
+
+// Import the model (.js) to use its database functions.
+
+
+router.get("/", function(req, res){
+// call function in models to pull data from mysql, get the newest entry, put it into infoObject, send to handlebarsjs
+    res.render("index", infoObject)
+});
+
+
+
+// POST method: get user input(the public image URL .jpg, .png) and send to KAIROS API, redirect to mainpage with displayed data
+router.post("/", function(req, res){
+
+    res.redirect("/");
+});
+
+
+
+// POST method: allow user to upload image(.jpg, .png), encode and send to KAIROS API, redirect to mainpage with displayed data
+router.post("/upload", function(req, res){
+//use formidable package to handle data upload, then use base64Img to encode image, save into base64.txt
+    res.redirect("/");
+});
