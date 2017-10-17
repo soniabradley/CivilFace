cec13dc9d5c79e2185e4231b54933c6c6044004c
DuyDo <duydo.dev@gmail.com>
2017-10-17 01:42:57 -0500

    use Billgate.png as local file

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
      connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "sangam0503",
+  password: "root",
   database: "civilface_DB"
 });
 };
